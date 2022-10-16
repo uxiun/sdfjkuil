54a9077c409b017e4093f721da53f2a0ce5e4a90
uxiun <teppeiito0905@gmail.com>
2022-10-16 14:41:33 +0900

    url prefixを設定する条件を変更

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production"
-const urlPrefix = isProd? '/' + process.env.URL_PREFIX : undefined // if "" instead of undefined: error
+// const isProd = process.env.NODE_ENV === "production"
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : undefined // if "" instead of undefined: error
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,

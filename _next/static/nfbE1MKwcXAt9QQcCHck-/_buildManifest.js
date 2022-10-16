77295355b2843e72d64fa4035f96e827bb119ccd
uxiun <uxiun@users.noreply.github.com>
2022-10-16 06:53:50 +0000

    deploy: a5454b7da209b421d89f8ebfa12321d637069b8c

--- _next/static/nfbE1MKwcXAt9QQcCHck-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-edcb5ecbc1676ef6.js"],"/_error":["static/chunks/pages/_error-fa2da97f07e0d864.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

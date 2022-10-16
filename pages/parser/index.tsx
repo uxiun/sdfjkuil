0d8fc4f6524700922426f23ac9cc37d03b6d0d99
uxiun <teppeiito0905@gmail.com>
2022-10-16 16:11:41 +0900

    Parser.tsx remove

---
+++ pages/parser/index.tsx
@@ -0,0 +1,10 @@
+import type {VFC} from "react"
+const Parser: VFC = () => {
+    return (
+        <div className="mainpage">
+            parser here
+        </div>
+    )
+}
+
+export default Parser
